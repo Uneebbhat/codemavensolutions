b9c8f2b5e50f74d9cace305ad74d87636e62f744
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-02-03 20:24:27 +0500

    fix: wrong spline export fixed

--- src/components/Home/MainSection.tsx
+++ src/components/Home/MainSection.tsx
@@ -1,36 +1,17 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import dynamic from "next/dynamic";
 import Button from "../common/Button";
 import Link from "next/link";
-
-// Lazy load Spline to improve performance
-const Spline = dynamic(() => import("@splinetool/react-spline"), {
-  ssr: false,
-  loading: () => <div className="w-full h-full bg-black-500"></div>, // Placeholder
-});
+import Spline from "@splinetool/react-spline";
 
 const MainSection = () => {
-  const [showSpline, setShowSpline] = useState(false);
-
-  // Delay Spline rendering to improve page speed
-  useEffect(() => {
-    const timer = setTimeout(() => setShowSpline(true), 2000); // Loads after 2s
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <main
       className="bg-black-500 text-white text-center min-h-[84vh] rounded-[20px] flex flex-col relative mt-[12px] px-[20px]"
       id="home"
     >
-      {/* Render Spline only when showSpline is true */}
-      {showSpline && (
-        <Spline
-          scene="https://prod.spline.design/F6nNb7ltNkvaT7ro/scene.splinecode"
-          className="absolute top-0 left-0 w-full h-full"
-        />
-      )}
+      <Spline
+        scene="https://prod.spline.design/F6nNb7ltNkvaT7ro/scene.splinecode"
+        className="absolute top-0 left-0 w-full h-full"
+      />
 
       <div className="relative z-10 flex flex-col flex-grow justify-end pb-[40px] gap-[20px]">
         <h1 className="text-h4 md:text-h1 font-medium">
