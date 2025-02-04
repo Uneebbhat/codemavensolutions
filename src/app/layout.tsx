f594bdeccc27c74354da13c191e45786f279c1da
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-02-04 11:18:52 +0500

    refactor: metadata updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,9 @@ import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
 
 export const metadata: Metadata = {
-  title: "Codemaven Solutions | We Build Your Dreams",
+  title: "Codemaven Solutions | Crafting Innovative Digital Solutions",
   description:
-    "Codemaven Solutions is a leading software development company specializing in web and mobile applications. We craft innovative, high-performance digital solutions that empower businesses to thrive in the digital era. Let's build your dream project together!",
+    "Codemaven Solutions is a cutting-edge software development company specializing in building high-quality web and mobile applications. Our team creates tailored digital experiences that empower businesses to excel in the digital world. Let’s turn your vision into reality and build the future together!",
 };
 
 export default function RootLayout({
