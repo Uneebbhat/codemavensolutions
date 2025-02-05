085e7dda226b2e4eb0bd67009d22ade899583a40
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-02-05 12:38:51 +0500

    metadata added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import "../styles/font-face.css";
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Codemaven Solutions | Crafting Innovative Digital Solutions",
@@ -18,6 +19,30 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-aspekta antialiased`}>
+        <Head>
+          <meta
+            property="og:title"
+            content="Codemaven Solutions | Crafting Innovative Digital Solutions"
+          />
+          <meta
+            property="og:description"
+            content="Codemaven Solutions is a cutting-edge software development company specializing in building high-quality web and mobile applications. Our team creates tailored digital experiences that empower businesses to excel in the digital world. Let’s turn your vision into reality and build the future together!"
+          />
+          <meta property="og:site_name" content="Codemaven Solutions" />
+          <meta
+            property="og:url"
+            content="https://codemavensolutions.vercel.app"
+          />
+          <script type="application/ld+json">
+            {JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              name: "Codemaven Solutions",
+              url: "https://codemavensolutions.vercel.app",
+              logo: "https://codemavensolutions.vercel.app/favicon.ico",
+            })}
+          </script>
+        </Head>
         <Header />
         {children}
         <Footer />
