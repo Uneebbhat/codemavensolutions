27c6978a5abc94b1130dfb0720f6ea18f5c13c33
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-02-09 11:29:22 +0500

    metadata changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,30 @@ import "./globals.css";
 import "../styles/font-face.css";
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Codemaven Solutions | Crafting Innovative Digital Solutions",
   description:
     "Codemaven Solutions is a cutting-edge software development company specializing in building high-quality web and mobile applications. Our team creates tailored digital experiences that empower businesses to excel in the digital world. Let’s turn your vision into reality and build the future together!",
+  openGraph: {
+    title: "Codemaven Solutions | Crafting Innovative Digital Solutions",
+    description:
+      "Codemaven Solutions is a cutting-edge software development company specializing in building high-quality web and mobile applications. Our team creates tailored digital experiences that empower businesses to excel in the digital world. Let’s turn your vision into reality and build the future together!",
+    url: "https://codemavensolutions.vercel.app",
+    siteName: "Codemaven Solutions",
+    type: "website",
+    images: [
+      {
+        url: "https://codemavensolutions.vercel.app/favicon.ico",
+        width: 1200,
+        height: 630,
+        alt: "Codemaven Solutions - Web & Mobile Development, UI/UX designing and Consultation Services",
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -19,30 +37,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-aspekta antialiased`}>
-        <Head>
-          <meta
-            property="og:title"
-            content="Codemaven Solutions | Crafting Innovative Digital Solutions"
-          />
-          <meta
-            property="og:description"
-            content="Codemaven Solutions is a cutting-edge software development company specializing in building high-quality web and mobile applications. Our team creates tailored digital experiences that empower businesses to excel in the digital world. Let’s turn your vision into reality and build the future together!"
-          />
-          <meta property="og:site_name" content="Codemaven Solutions" />
-          <meta
-            property="og:url"
-            content="https://codemavensolutions.vercel.app"
-          />
-          <script type="application/ld+json">
-            {JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "Organization",
-              name: "Codemaven Solutions",
-              url: "https://codemavensolutions.vercel.app",
-              logo: "https://codemavensolutions.vercel.app/favicon.ico",
-            })}
-          </script>
-        </Head>
         <Header />
         {children}
         <Footer />
